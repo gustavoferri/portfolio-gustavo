2ed09b0fe788d63899d7000572a7635074092556
gustavoferri <eu@gustavoferri.com.br>
2019-01-30 21:24:37 -0300

    Bootstrap Integration

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,23 @@
 import React from 'react';
 import BaseLayout from '../components/layouts/BaseLayout'
-import axios from 'axios';
 
-import SuperComponent from '../components/SuperComponent';
+import { Button } from 'reactstrap';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-class Index extends SuperComponent {
-
-    static async getInitialProps() {
-        let userData = {};
-
-        try {
-        const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
-        userData = response.data;
-        } catch(err) {
-            console.error(err);
-        }
-
-        return {initialData: [1,2,3,4], userData};
-    }
-
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            title: 'Eu sou a Página Inicial'
-        }
-    }
-    componentDidMount() {
-        console.log('ComponentDidMount');
-    }
-    componentDidUpdate() {
-        console.log('componentDidUpdate');
-    }
-    componentWillUnmount() {
-        console.log('componentWillUnmount');
-    }
-    updateTitle = () => {
-        // console.log('hehehehehheh');
-        this.setState({title: 'Eu sou a Página Inicial Atualizada'});
-    }
+class Index extends React.Component  {
 
     render() {
-        const { title } = this.state;
-        // const initialData =  this.props.initialData;
-        const { userData, initialData } = this.props;
-
 
       return (
          <BaseLayout>
-            <h1 className='frontPage'> Minha Página Index.js da Classe de Componentes </h1>
-            <h2> { title } </h2>
-            <h2> { userData.title } </h2>
-            <button onClick={this.updateTitle}> Mudar Título </button>
+            <Container>
+                <Button color="danger">Danger!</Button>
+            </Container>
         </BaseLayout>
       )
    }
 }
 
-export default Index;
 
-{/* <Header title={'Eu sou um componente header'}>
-<h1> I sou um header subtitulo</h1>
-</Header> */}
+export default Index;
