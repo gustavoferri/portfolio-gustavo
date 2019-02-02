41e5b1f99ec1eb345d670a87a2ec496d4679b4a7
gustavoferri <eu@gustavoferri.com.br>
2019-02-02 16:42:28 -0300

    Handle Authentication, Callback Page and Save Tokens in Cookies

---
+++ pages/callback.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import BaseLayout from '../components/layouts/BaseLayout';
+
+import auth0Client from '../services/auth0';
+import { withRouter } from 'next/router';
+
+class Callback extends React.Component {
+
+    async componentDidMount() {
+        await auth0Client.handleAuthentication();
+        this.props.router.push('/');
+    }
+
+    render() {
+      return (
+          <BaseLayout>
+            <h1> Verifying login  data ... </h1>
+          </BaseLayout>
+        
+      )
+   }
+}
+
+export default withRouter (Callback);
--- services/auth0.js
+++ services/auth0.js
@@ -1,6 +1,9 @@
 // src/Auth/Auth.js
 
 import auth0 from 'auth0-js';
+import Cookies from 'js-cookie';
+import { resolve } from 'dns';
+import { rejects } from 'assert';
 
 class Auth0 {
 
@@ -14,11 +17,59 @@ class Auth0 {
           });
 
           this.login = this.login.bind(this);
+          this.logout = this.logout.bind(this);
+          this.handleAuthentication = this.handleAuthentication.bind(this);
+          this.isAuthenticated = this.isAuthenticated.bind(this);
         }
+
+        handleAuthentication() {
+            this.auth0.parseHash((err, authResult) => {
+              if (authResult && authResult.accessToken && authResult.idToken) {
+                this.setSession(authResult);
+                resolve();
+              } else if (err) {
+                  rejects(err);
+                console.log(err);
+                alert(`Error: ${err.error}. Check the console for further details.`);
+              }
+            });
+          }
+
+     setSession(authResult) {
+         const expiresAt = JSON.stringify((authResult.expiresIn = 1000) + new Date().getTime());
+
+        //  localStorage.setItem('id_token', authResult.idToken);
+
+        Cookies.set('user', authResult.idTokenPayload);
+        Cokkies.set('jwt', authResult.idToken);
+        Cokkies.set('expiresAt', expiresAt);
+    }
+
+    logout() {
+        Cookies.remove('user');
+        Cokkies.remove('jwt');
+        Cokkies.remove('expiresAt');
+
+        this.auth0.logout({
+            returnTo: '',
+            clientID: '7mc3hlqKkLNS9oJJwgL2QAKM7yIfU7cR'
+        })
+    }
+        
 
   login() {
     this.auth0.authorize();
   }
+
+  isAuthenticated() {
+    // Check whether the current time is past the
+    // access token's expiry time
+    const expiresAt = Cookies.getJson('expiresAt');
+    return new Date().getTime() < expiresAt;
+  }
+}
+
+
 }
 
 const auth0Client = new Auth0();
