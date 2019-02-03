679dfd0d71e32f1069aea7ecb581f9ad1752bec0
gustavoferri <eu@gustavoferri.com.br>
2019-02-03 20:43:53 -0300

    Secret Endpoint Server

--- pages/secret.js
+++ pages/secret.js
@@ -4,12 +4,38 @@ import BasePage from '../components/BasePage';
 
 import withAuth from '../components/hoc/withAuth';
 
+import axios from 'axios';
+
 class Secret extends React.Component {
 
     getInitialProps() {
         const superSecretValue = 'Super Secret Value';
 
         return { superSecretValue };
+    }
+
+    async componentDidMount() {
+        const res = await axios.get('/api/v1/secret');
+        const secretData = res.data;
+        this.setState({
+            secretData
+        });
+    }
+
+    displaySecretData() {
+       const { secretData } = this.state;
+       
+       if ( secretData && secretData.length > 0) {
+           return secretData.map((data, index) => {
+               return (
+                   <div key={index}>
+                    <p> { data.title } </p>
+                    <p> {data.description } </p>
+                   </div>
+               )
+           })
+       }
+       return null;
     }
 
     render() {
