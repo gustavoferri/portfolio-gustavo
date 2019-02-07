edef865c3458643591a8fca7d2ddaf761d366b79
gustavoferri <eu@gustavoferri.com.br>
2019-02-07 20:38:27 -0300

    Portfolio Card Styling

--- pages/portfolios.js
+++ pages/portfolios.js
@@ -2,6 +2,7 @@ import React from 'react';
 import BaseLayout from '../components/layouts/BaseLayout';
 import BasePage from '../components/BasePage';
 import { Link } from '../routes'
+import { Col, Row, Card, CardHeader, CardBody, CardText, CardTitle } from 'reactstrap';
 
 import axios from 'axios';
 
@@ -24,11 +25,21 @@ class Portfolios extends React.Component {
     renderPosts(posts) {
         return posts.map((post, index) => {
             return (
-                <li key={index}> 
-                    <Link route={`/portfolio/${post.id}`}>
-                         <a style={{'fontSize': '20px'}}> {post.title} </a>
-                    </Link>
-                </li>
+                <Col md="4">
+                    <React.Fragment key={index}>
+                        <span>
+                            <Card className="portfolio-card">
+                            <CardHeader className="portfolio-card-header">Some Position {index}</CardHeader>
+                            <CardBody>
+                                <p className="portfolio-card-city"> Some Location {index} </p>
+                                <CardTitle className="portfolio-card-title">Some Company {index}</CardTitle>
+                                <CardText className="portfolio-card-text">Some Description {index}</CardText>
+                                <div className="readMore"> </div>
+                            </CardBody>
+                            </Card>
+                        </span>
+                    </React.Fragment>
+              </Col>
             )
         })
     }
@@ -38,11 +49,10 @@ class Portfolios extends React.Component {
         
         return (
           <BaseLayout {...this.props.auth}>
-            <BasePage title="Portfolio">
-                <h1> Minha Página Portfolios </h1>
-                <ul>
+            <BasePage className="portfolio-page" title="Portfolio">
+                <Row>
                     { this.renderPosts(posts) }
-                </ul>
+                </Row>
             </BasePage>
           </BaseLayout>
       )
