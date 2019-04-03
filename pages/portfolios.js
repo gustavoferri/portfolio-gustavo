4535113969c2a6420163087767a1c677bfe58579
gustavoferri <eu@gustavoferri.com.br>
2019-04-02 23:51:33 -0300

    Stop Propagation on Click

--- pages/portfolios.js
+++ pages/portfolios.js
@@ -24,7 +24,13 @@ class Portfolios extends React.Component {
         return {portfolios};
     }
 
-    displayDeleteWarning(portfolioId) {
+    navigateToEdit(portfolioId, e) {
+        e.stopPropagation();
+        Router.pushRoute(`/portfolios/${portfolioId}/edit`)
+    }
+
+    displayDeleteWarning(portfolioId, e) {
+        e.stopPropagation();
        const isConfirm = confirm('Você esta prestes a deletar o portfolio, tem certeza disso???');
 
        if(isConfirm){
@@ -50,8 +56,8 @@ class Portfolios extends React.Component {
                     <PortfolioCard portfolio={portfolio}>
                     { isAuthenticated && isSiteOwner &&
                            <React.Fragment>
-                              <Button onClick={() => Router.pushRoute(`/portfolios/${portfolio._id}/edit`)} color="warning">Edit</Button> {''}
-                              <Button onClick={() => this.displayDeleteWarning(portfolio._id)} color="danger">Delete</Button>
+                              <Button onClick={(e) => this.navigateToEdit(portfolio._id, e)} color="warning">Edit</Button> {''}
+                              <Button onClick={(e) => this.displayDeleteWarning(portfolio._id, e)} color="danger">Delete</Button>
                            </React.Fragment>
                         }
                     </PortfolioCard>
