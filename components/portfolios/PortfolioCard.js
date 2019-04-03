fa7b445e5cca9bea1945e5ae2a4c0523a8c603de
gustavoferri <eu@gustavoferri.com.br>
2019-04-02 23:27:47 -0300

    Fill Modal with Portfolio Data

--- components/portfolios/PortfolioCard.js
+++ components/portfolios/PortfolioCard.js
@@ -5,15 +5,29 @@ import PortfolioCardDetail from './PortfolioCardDetail';
 export default class PortfolioCard extends React.Component {
 
     constructor(props) {
-        super();
-    }
+        super(props);
+        this.state = {
+          isOpen: false
+        };
+    
+        this.handleToggle = this.handleToggle.bind(this);
+      }
+    
+      handleToggle() {
+        this.setState({
+           isOpen: !this.state.isOpen
+        });
+      }
 
     render() {
         const { portfolio, children } = this.props;
+        const { isOpen } = this.state;
 
         return (
-            <span>
-                <PortfolioCardDetail />
+            <span onClick={this.handleToggle}>
+
+                <PortfolioCardDetail toggle={this.handleToggle} portfolio={portfolio} isOpen={isOpen} />
+
                 <Card className="portfolio-card">
                 <CardHeader className="portfolio-card-header">{portfolio.position}</CardHeader>
                     <CardBody>
