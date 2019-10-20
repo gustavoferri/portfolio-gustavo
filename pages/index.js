55108d01f2e6d4fad0e44c460cab8abcf3ea9a11
gustavoferri <eu@gustavoferri.com.br>
2019-10-19 21:48:53 -0300

    Flipping Card part 2

--- pages/index.js
+++ pages/index.js
@@ -9,18 +9,14 @@ class Index extends React.Component  {
 
     constructor(props) {
         super(props);
-
         this.state = {
             isFlipping: false
         }
-
         this.roles = ['Developer', 'Mobile Specialist', 'React.js', 'JavaScript', 'Magento'];
     }
-
     componentDidMount() {
         this.animateCard();
     }
-
     componentWillLeave() {
         this.cardAnimationInterval && clearInterval(this.cardAnimationInterval);
     }
@@ -31,70 +27,78 @@ class Index extends React.Component  {
             });
         }, 3000);
     }
-
     render() {
         const { isAuthenticated, user } =  this.props.auth;
         const { isFlipping } = this.state;
 
-      return (
-        <BaseLayout className="cover" {...this.props.auth} headerType="index">
-                <div className="main-section">
-                    <div className="background-image">
-                    <img src="/static/images/background-index.png" />
-                 </div>
-            <Container>
-                <Row>
-                <Col md="6">
-                    <div className="hero-section">
+    return (
+      <BaseLayout className="cover" {...this.props.auth} headerType="index">
+        <div className="main-section">
+          <div className="background-image">
+             <img src="/static/images/background-index.png" />
+          </div>
+          <Container>
+            <Row>
+              <Col md="6">
+                 <div className="hero-section">
                     <div className={`flipper ${isFlipping ? 'isFlipping' : ''}`}>
-                        <div className="back">
-                        <div className="hero-section-content">
+                       <div className="front">
+                          <div className="hero-section-content">
                             <h2> Full Stack Web Developer </h2>
                             <div className="hero-section-content-intro">
-                            Have a look at my portfolio and job history.
+                              Conheça alguns dos meus trabalhos realizados.
                             </div>
-                        </div>
-                        <img className="image" src="/static/images/section-1.png"/>
-                        <div className="shadow-custom">
-                            <div className="shadow-inner"> </div>
-                        </div>
-                        </div>
+                          </div>
+                          <img className="image" src="/static/images/section-1.png"/>
+                          <div className="shadow-custom">
+                             <div className="shadow-inner"> </div>
+                          </div>
+                       </div>
+                       <div className="back">
+                       <div className="hero-section-content">
+                         <h2> Tire Seu Projeto Do Papel </h2>
+                         <div className="hero-section-content-intro">
+                             Serviço de alta qualidade feito por profissional.
+                         </div>
+                       </div>
+                       <img className="image" src="/static/images/section-2.png"/>
+                       <div className="shadow-custom">
+                          <div className="shadow-inner"> </div>
+                       </div>
                     </div>
                     </div>
-                </Col>
-                <Col md="6" className="hero-welcome-wrapper">
-                    <div className="hero-welcome-text">
+                 </div>
+              </Col>
+              <Col md="6" className="hero-welcome-wrapper">
+                <div className="hero-welcome-text">
                     <h1>
-                        { isAuthenticated && <span> <b> {user.name} </b> </span> }
-                        Welcome to the portfolio website of Gustavo Ferri.
-                        Get informed, collaborate and discover projects I was working on through the years!
+                    { isAuthenticated && <span> <b> {user.name} </b> </span> }
+                    Welcome to the portfolio website of Gustavo Ferri.
+                    Get informed, collaborate and discover projects I was working on through the years!
                     </h1>
-                    </div>
-                     <Typed
-                        loop
-                        typeSpeed={60}
-                        backSpeed={60}
-                        strings={this.roles}
-                        backDelay={1000}
-                        loopCount={0}
-                        showCursor
-                        className="self-typed"
-                        cursorChar="|"
-                    />
-
-
-                    <div className="hero-welcome-bio">
+                </div>
+                <Typed
+                  loop
+                  typeSpeed={60}
+                  backSpeed={60}
+                  strings={this.roles}
+                  backDelay={1000}
+                  loopCount={0}
+                  showCursor
+                  className="self-typed"
+                  cursorChar="|"
+                />
+                <div className="hero-welcome-bio">
                     <h1>
                         Let's take a look on my work.
                     </h1>
-                    </div>
-                </Col>
-                </Row>
-            </Container>
-            </div>
-        </BaseLayout>
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+       </BaseLayout>
       )
    }
 }
-
 export default Index;
