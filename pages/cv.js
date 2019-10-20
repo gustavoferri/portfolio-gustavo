6324b78e94cebc755415bf81306dc3cff8746e75
gustavoferri <eu@gustavoferri.com.br>
2019-10-20 19:36:56 -0300

    CV Page

--- pages/cv.js
+++ pages/cv.js
@@ -2,14 +2,25 @@ import React from 'react';
 import BaseLayout from '../components/layouts/BaseLayout';
 import BasePage from '../components/BasePage';
 
+import { Row, Col } from 'reactstrap';
 
 class Cv extends React.Component {
 
     render() {
       return (
           <BaseLayout {...this.props.auth}>
-            <BasePage>
-               <h1>Minha Página Cv</h1>
+            <BasePage title="Pré-visualização do meu Currículo" className="cv-page">
+              <Row>
+                <Col md={{size: 8, offset: 2}}>
+                  <div className="cv-title">
+                     <a download="gustavo_cv.pdf" className="btn btn-success" href="/static/gustavo_cv.pdf">
+                       Download
+                     </a>
+                  </div>
+                  <iframe style={{width: '100%', height: '800px'}} src="/static/gustavo_cv.pdf">
+                  </iframe>
+                </Col>
+              </Row>
             </BasePage>
           </BaseLayout>
       )
